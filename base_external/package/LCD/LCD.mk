7d1391b298d5b77e965f080c0bf81e1cb6b99437
admin <admin@MARK-PC-VM.myguest.virtualbox.org>
2023-04-16 21:01:09 -0400

    updated LCD makefile, added package

--- base_external/package/LCD/LCD.mk
+++ base_external/package/LCD/LCD.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LCD_VERSION = '81a0610d4131fc79cbc9c6b258cc8e4c04dfef24'
+LCD_VERSION = '990472809abe91e5526257a19e90ae200d5e8142'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,12 +15,12 @@ LCD_SITE_METHOD = git
 LCD_GIT_SUBMODULES = YES
 
 define LCD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/src all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/src
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define LCD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/src/LCD $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/src/main $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
